57962c14d098c9930dfc6e1ef87c5e8ebd1a96a9
jeremyacoop <63517273+jeremyacoop@users.noreply.github.com>
2022-02-08 22:03:23 -0600

    Set up sessions router, plugged into server

---
+++ server/routes/sessions.router.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  // GET route code here
+  console.log('In router GET');
+  const queryText = `SELECT * FROM "sessions";`;
+  pool.query(queryText)
+  .then((result) => {
+    console.log(result);
+    res.send(result.rows);
+  })
+  .catch((err) => {
+    console.log('Cannot retrieve sessions from db.', err);
+    res.sendStatus(500);
+  })
+});
+
+/**
+ * POST route template
+ */
+router.post('/', (req, res) => {
+  // POST route code here
+});
+
+module.exports = router;
+
+
--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const passport = require('./strategies/user.strategy');
 // Route includes
 const userRouter = require('./routes/user.router');
 const bookmarksRouter = require('./routes/bookmarks.router.js');
+const sessionsRouter = require('./routes/sessions.router.js');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -25,6 +26,7 @@ app.use(passport.session());
 /* Routes */
 app.use('/api/user', userRouter);
 app.use('/bookmarks', bookmarksRouter);
+app.use('/sessions', sessionsRouter);
 
 // Serve static files
 app.use(express.static('build'));
