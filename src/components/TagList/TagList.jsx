bbcf3bae2a113d9d1abc61d2a56795eb758ea059
jeremyacoop <63517273+jeremyacoop@users.noreply.github.com>
2022-02-10 11:17:05 -0600

    Created TagList component

---
+++ src/components/TagList/TagList.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+
+function TagList() {
+    const dispatch = useDispatch();
+    const tags = useSelector((store) => store.tags);
+    const [heading, setHeading] = useState('Tags');
+    const [tagName, setTagName] = useState('');
+
+    const fetchTags = () => {
+        console.log('In fetchTags');
+        dispatch({
+            type:   'FETCH_TAGS',
+        });
+    }
+
+    useEffect(() => {
+        fetchTags();
+    }, []);
+
+    const deleteTag = (id) => {
+        console.log('In deleteTag', id);
+        dispatch({
+            type:   'DELETE_TAGS',
+            id: id
+        });
+    }
+
+    return (
+        <section>
+            <div>
+            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>
+                            <h2>{heading}</h2>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody className="tag-list">
+                    {tags.map((tag) => (
+                        <tr key={tag.id}>
+                            <td>{tag.title}</td>
+                            <td><button onClick={evt => deleteTag(tag.id)}>X</button></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+        </section>
+    );
+}
+
+export default TagList;
+
+
